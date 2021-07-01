3ab528591c5b57a6cf9ade7041602e87b79715b5
Eke Victor <evavic44@gmail.com>
2021-07-01 05:54:04 +0100

    Add Tawk chatbox

--- js/script.js
+++ js/script.js
@@ -32,3 +32,15 @@ const navbar = document.getElementById('navbar');
          );
       }
    });
+
+// Start of Tawk.to Script
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/60dd436c65b7290ac638d1d9/1f9g4uo63';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+// End of Tawk.to Script
